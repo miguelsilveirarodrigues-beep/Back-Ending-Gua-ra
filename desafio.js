08f4f81091b458d0c3fe4a38aaf4598d2a619a41
MiguelRodrigues <miguel.silveira.rodrigues@escola.pr.gov.br>
2026-05-18 19:24:14 +0000

    versao2

---
+++ desafio.js
@@ -0,0 +1,12 @@
+const nome = "Cleber";
+let cargo = "Gerente"
+let idade = 15;
+let salario = 6000.00;
+let empresa = "Assai";
+
+console.log(`Qual seu nome? ${nome} Oque quer fazer na empresa? ${cargo} Qual sua idade? ${idade} Quanto quer receber? ${salario} Qual sua empresa? ${empresa}`);
+
+let bônus = 10000.00;
+let soma = salario + bônus;
+
+console.log(`a soma total do salario do meu gerente com o bonus é (soma)`)
